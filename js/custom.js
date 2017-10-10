d3386d2127eec8d8acd76d88baaad575fecc5019
dumas <elodie.dumasmarliens@gmail.com>
2017-10-10 11:47:48 +0200

    animation home page

--- js/custom.js
+++ js/custom.js
@@ -9,4 +9,29 @@ jQuery(document).ready(function()
 	{
 		$('.navbar').toggle('slow');
 	})
+
+	//fonction pour taper la citation
+	text = 'S.A.R.L';
+	emplacement = '#title';
+	textfrappe(text, emplacement);
+
 });
+
+
+function textfrappe($text, $emplacement)
+{
+  setTimeout(function()
+  {
+    $($emplacement).typed(
+    {
+        strings: [$text], // More SEO Friendly look here: https://github.com/mattboldt/typed.js/
+        typeSpeed: 150, // typing speed
+        backDelay: 750, // pause before backspacing
+        // loop: true, // loop on or off (true or false)
+        // loopCount: false, // number of loops, false = infinite
+        // callback: function(){ } // call function after typing is done
+    });
+  },
+    0
+  );
+}
