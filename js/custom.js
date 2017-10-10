bbd2ddf4a15bdd260efe5a00cf7fa0aaddbb97d7
dumas <elodie.dumasmarliens@gmail.com>
2017-10-10 11:56:06 +0200

    animation home_page

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,33 @@ jQuery(document).ready(function()
 	text = 'S.A.R.L';
 	emplacement = '#title';
 	textfrappe(text, emplacement);
+	//fin
+
+
+	window.sr = ScrollReveal();
+
+	sr.reveal('.bloc',
+	{
+		origin: 'bottom',
+		distance: '110px',
+		scale: '1',
+		easing: 'ease',
+		delay: 800,
+		duration: 2000,
+	}, 0);
+
+	($('.section_2').children()).each(function(index)
+	{
+		sr.reveal(this,
+		{
+		origin: 'left',
+		distance: '110px',
+		scale: '1',
+		easing: 'ease',
+		delay: 300,
+		duration: 1200,
+		}, 300);
+	});
 
 });
 
@@ -25,8 +52,9 @@ function textfrappe($text, $emplacement)
     $($emplacement).typed(
     {
         strings: [$text], // More SEO Friendly look here: https://github.com/mattboldt/typed.js/
-        typeSpeed: 150, // typing speed
+        typeSpeed: 200, // typing speed
         backDelay: 750, // pause before backspacing
+        showCursor: false,
         // loop: true, // loop on or off (true or false)
         // loopCount: false, // number of loops, false = infinite
         // callback: function(){ } // call function after typing is done
